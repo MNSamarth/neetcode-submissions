4f1c80060429b8e61e5b65193556cb55f04f3062
Samarth Narahari Mandagere <91833600+MNSamarth@users.noreply.github.com>
2026-09-20 18:49:24 -0700

    Add: valid-binary-search-tree - submission-0

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-0.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+class Solution:
+    def isValidBST(self, root: TreeNode | None) -> bool:
+        if not root:
+            return True
+
+        q = deque()
+        q.append([root, float("-inf"), float("inf")])
+
+        while q:
+            node, low, high = q.popleft()
+
+            if not (low < node.val < high):
+                return False
+
+            if node.left:
+                q.append([node.left, low, node.val])
+
+            if node.right:
+                q.append([node.right, node.val, high])
+
+        return True
